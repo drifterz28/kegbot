62c521142c1ada67ae2c38319f5ea6ffe1edb9a0
Chris Whitney <cwhitney@goldstar.com>
2016-10-19 22:29:10 -0700

    work on second server saving variables

--- arduino/kegbot/kegbot.ino
+++ arduino/kegbot/kegbot.ino
@@ -1,18 +1,15 @@
 // ***Use the AVRISP mkII programer***
-#include <ArduinoJson.h>
-#include "FS.h"
-
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
 #include <WebSocketsServer.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
+#include <ArduinoJson.h>
+#include "FS.h"
+
 #include "HX711.h"
 #include "DHT.h"
-
-const char* defaultSsid = "KegBot";
-const char* defaultPassword = "admin";
 
 const char* ssid = "";
 const char* password = "";
@@ -43,49 +40,6 @@ HX711 hx711_1;
 #define hx711_data_pin2 4
 #define hx711_clock_pin2 5
 HX711 hx711_2;
-
-bool loadConfig() {
-  File configFile = SPIFFS.open("/config.json", "r");
-  if (!configFile) {
-    Serial.println("Failed to open config file");
-    return false;
-  }
-  size_t size = configFile.size();
-  if (size > 1024) {
-    Serial.println("Config file size is too large");
-    return false;
-  }
-  std::unique_ptr<char[]> buf(new char[size]);
-
-  configFile.readBytes(buf.get(), size);
-
-  StaticJsonBuffer<200> jsonBuffer;
-  JsonObject& json = jsonBuffer.parseObject(buf.get());
-
-  if (!json.success()) {
-    Serial.println("Failed to parse config file");
-    return false;
-  }
-  defaultAPssid = json["ssid"];
-  defaultAPpassword = json["password"];
-  return true;
-}
-
-bool saveConfig() {
-  StaticJsonBuffer<200> jsonBuffer;
-  JsonObject& json = jsonBuffer.createObject();
-  json["ssid"] = defaultSsid;
-  json["password"] = defaultPassword;
-
-  File configFile = SPIFFS.open("/config.json", "w");
-  if (!configFile) {
-    Serial.println("Failed to open config file for writing");
-    return false;
-  }
-
-  json.printTo(configFile);
-  return true;
-}
 
 float getHumidity() {
   float humidity = dht.readHumidity();
@@ -148,16 +102,8 @@ void handleRoot() {
 }
 
 void handleSetup() {
+  Serial.println("setup");
   server.send(200, "text/html", "<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>KegBot Setup</title></head><body><form method=\"post\" action=\"/save\" style=\"margin: 0 auto;max-width:500px;width:100%;\"><h3>Wifi setup</h3><table><tr><td><label>SSID:</label></td><td><input name=\"ssid\"/></td></tr><tr><td><label>Password:</label></td><td><input name=\"password\"/></td></tr></table><button>Save</button></form></body></html>");
-}
-
-void handleSave() {
-  // list all options to change and make a check for arg and update arg to file.
-  // return status. 400 if fail and 200 if OK.
-  // example to get args
-  // server.hasArg("argname");
-  // server.arg("argname");
-  server.send(200, "text/html", "some html");
 }
 
 void handleHome() {
@@ -166,7 +112,6 @@ void handleHome() {
 
 void setup() {
   Serial.begin(115200);
-
   for (uint8_t t = 4; t > 0; t--) {
     Serial.printf("[SETUP] BOOT WAIT %d...\n", t);
     Serial.flush();
@@ -174,7 +119,7 @@ void setup() {
   }
 
   WiFiMulti.addAP(ssid, password);
-
+  WiFi.softAP("esp8266", "nothingyoucanfindout", 1, 1);
   while(WiFiMulti.run() != WL_CONNECTED) {
     delay(100);
   }
@@ -186,21 +131,19 @@ void setup() {
   if(MDNS.begin("esp8266")) {
     Serial.println("MDNS responder started");
   }
-  // keeps from sending ssid for connection
-  WiFi.softAP("KegBot", password);
+
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
   // handle routes
   server.on("/", handleRoot);
-  server.on("/setup", handleSetup);
-  server.on("/save", handleSave);
   server.on("/home", handleHome);
   server.begin();
 
   // Add service to MDNS
   MDNS.addService("http", "tcp", 80);
   MDNS.addService("ws", "tcp", 81);
+  Serial.println("Begin scales and dht");
 
   hx711_1.begin(hx711_data_pin1, hx711_clock_pin1);
   hx711_2.begin(hx711_data_pin2, hx711_clock_pin2);
@@ -211,6 +154,7 @@ void setup() {
   hx711_2.set_scale(scaleReset);
   hx711_1.tare();
   hx711_2.tare();
+  Serial.println("done with setup");
 }
 
 void loop() {
--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,18 @@
 const url = '192.168.0.108'; //location.hostname;
+const kegBotUrl = 'https://iot-ecommsolution.rhcloud.com/kegbot';
 const conn = new WebSocket(`ws://${url}:81/`, ['arduino']);
 
 const utils = {
   precentOfMax: (amount, maxAmount) => {
+    if(amount === 0 && maxAmount === 0) {
+      return 0;
+    }
     return (amount / maxAmount) * 100;
+  },
+  updateName: (kegNumber, kegName) => {
+    return fetch(`${kegBotUrl}?keg=${kegNumber}&name=${kegName}`)
+      .then(response => response.json())
+      .then(json => json);
   }
 };
 
@@ -31,11 +40,16 @@ const Keg = React.createClass({
     });
     this.props.handleSave(this.state.kegName, this.props.kegNumber);
   },
+  handleNewMaxValue() {
+    this.props.handleNewMaxValue(this.props.kegNumber);
+  },
   render() {
-    const {value} = this.props;
+    const {value, maxValue} = this.props;
     const {kegName} = this.state;
+    const max = (maxValue < value)? value : maxValue;
+    const precentage = Math.floor(utils.precentOfMax(value, max));
     const styles = {
-      "transform": 'translateY(-' + value + '%)'
+      "transform": 'translateY(-' + precentage + '%)'
     };
     return (
       <section className="kegWrapper">
@@ -46,8 +60,8 @@ const Keg = React.createClass({
           </form>
         }
         <div className="kegGraph">
-          <div className="keg">
-            {Math.ceil(value)}%
+          <div className="keg" onDoubleClick={this.handleNewMaxValue}>
+            {precentage}%
           </div>
           <div className="progress" style={styles}></div>
         </div>
@@ -74,12 +88,14 @@ const TempAndHumidity = function({temp, humidity}) {
 const App = React.createClass({
   getInitialState() {
     return {
-      "temp": 0,
-      "humidity": 0,
-      "kegOne": 0,
-      "kegTwo": 0,
-      "kegOneName": 'dbl click to add name',
-      "kegTwoName": 'dbl click to add name'
+      temp: 0,
+      humidity: 0,
+      kegOne: 0,
+      kegTwo: 0,
+      kegOneName: 'dbl click to add name',
+      kegTwoName: 'dbl click to add name',
+      kegOneMaxValue: 0,
+      kegTwoMaxValue: 0
     };
   },
   handleError(err) {
@@ -98,24 +114,25 @@ const App = React.createClass({
   },
   handleUpdate(data) {
     const json = JSON.parse(data);
+    console.log(json)
     this.setState(json);
   },
   handleSave(value, number) {
-    let newState = {};
-    if(number === 1) {
-      newState.kegOneName = value;
-    } else {
-      newState.kegTwoName = value;
-    }
+    const kegNumber = (number === 1) ? 'kegOne' : 'kegTwo';
+    const newState = utils.updateName(kegNumber, value);
+    console.log(newState)
     this.setState(newState);
   },
+  handleNewMaxValue(kegNumber) {
+    console.log(kegNumber);
+  },
   render() {
-    const {humidity, kegOne, kegOneName, kegTwo, kegTwoName, temp} = this.state;
+    const {temp, humidity, kegOne, kegTwo, kegOneName, kegTwoName, kegOneMaxValue, kegTwoMaxValue} = this.state;
     return (
       <div className="app">
-        <Keg name={kegOneName} value={kegOne} kegNumber='1' handleSave={this.handleSave}/>
+        <Keg name={kegOneName} value={kegOne} kegNumber={1} maxValue={kegOneMaxValue} handleSave={this.handleSave} handleNewMaxValue={this.handleNewMaxValue}/>
         <TempAndHumidity temp={temp} humidity={humidity}/>
-        <Keg name={kegTwoName} value={kegTwo} kegNumber='2' handleSave={this.handleSave}/>
+        <Keg name={kegTwoName} value={kegTwo} kegNumber={2} maxValue={kegTwoMaxValue} handleSave={this.handleSave} handleNewMaxValue={this.handleNewMaxValue}/>
       </div>
     );
   }
